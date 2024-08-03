47207428a06b757939d585ef1ecdd7013e0bb769
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-08-03 11:35:33 +0600

    f

--- cli.spec
+++ cli.spec
@@ -22,7 +22,7 @@ exe = EXE(
     a.binaries,
     a.datas,
     [],
-    name='__main__',
+    name='shiinobi',
     debug=False,
     bootloader_ignore_signals=False,
     strip=False,
--- src/shiinobi/cli.py
+++ src/shiinobi/cli.py
@@ -2,12 +2,15 @@ import typer
 import json
 from io import BytesIO
 import base64
-from typing import Literal
+
+from typing import Literal, Optional
+from typing_extensions import Annotated
 
 from datetime import datetime
 from shiinobi.utilities.session import session
 from shiinobi.builder.staff import StaffBuilder
 from shiinobi.parser.staff import StaffParser
+from shiinobi.builder.anime_theme import AnimeThemeBuilder
 
 app = typer.Typer()
 
@@ -30,14 +33,26 @@ def get_url_given_key_and_id(key: Literal["people"], mal_id: int) -> session:
 
 
 @app.command()
+def get_anime_themes():
+    builder = AnimeThemeBuilder()
+    dictionary = builder.build_dictionary()
+    print_json(dictionary)
+
+
+@app.command()
 def get_specific_staff_information(staff_id: int):
     url = get_url_given_key_and_id("people", staff_id)
     print_json(StaffParser(url.text).build_dictionary())
 
 
 @app.command()
-def get_staff_dict():
-    print_json(StaffBuilder().build_dictionary())
+def get_staff_dict(
+    excluded_ids: Annotated[Optional[list[int]], typer.Option()] = [],
+    sort: Annotated[Optional[bool], typer.Option()] = False,
+):
+    builder = StaffBuilder()
+    dictionary = builder.build_dictionary(excluded_ids=excluded_ids, sort=sort)
+    print_json(dictionary)
 
 
 if __name__ == "__main__":
