02ad29b14c6c7c1c1b7d9f76041d79cead24c8ae
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2025-01-14 13:47:26 +0600

    Change expire times

--- src/shiinobi/constants/session.py
+++ src/shiinobi/constants/session.py
@@ -1,4 +1,6 @@
-RETRY_STATUSES = [403, 429]
+from datetime import timedelta
+
+RETRY_STATUSES = [403, 429, 400]
 TOTAL_RETRIES = 15
 BACKOFF_FACTOR = 2
-EXPIRE_AFTER = 360
+EXPIRE_AFTER = int(timedelta(hours=1).total_seconds())
